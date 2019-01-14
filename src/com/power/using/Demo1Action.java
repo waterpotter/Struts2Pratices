bd88130317aa1c1b59e92191aea339194e2bc6ab
hashiqi <lh1357ke@163.com>
2019-01-14 09:03:06 +0800

    sixth commit

---
+++ src/com/power/using/Demo1Action.java
@@ -0,0 +1,16 @@
+package com.power.using;
+
+import com.opensymphony.xwork2.ActionSupport;
+
+public class Demo1Action extends ActionSupport {
+	
+	@Override
+	public String execute() throws Exception {
+		// TODO Auto-generated method stub
+		return SUCCESS;
+	}
+	
+	
+	
+
+}
